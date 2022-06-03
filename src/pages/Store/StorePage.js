9099c5f76ced86d1599138864fba83721eb3f6e3
Garrett Gordon <gagordon@mit.edu>
2022-06-03 02:50:14 -0400

    updating store products

--- src/pages/Store/StorePage.js
+++ src/pages/Store/StorePage.js
@@ -1,8 +1,29 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function StorePage() {
+
+  const [store, setStore] = useState([{"name" : "Elvis"}]);
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('/products')
+    .then((resp) => {
+      setStore(resp.data);
+      setLoading(false);
+      console.log(resp.data);
+    });
+
+  }, [setStore]);
   return (
-    <div> Store </div>
+    <div>
+
+      {store.map(item => <li key={item.id}> {item.name} </li> )}
+
+    </div>
+
   )
 
 }
