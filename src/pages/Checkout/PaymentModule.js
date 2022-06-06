796d5f7b980930e9141006a8ec2fb27587d53c9a
Garrett Gordon <gagordon@mit.edu>
2022-06-06 00:07:57 -0400

    wrapped react app w/ stripe

--- src/pages/Checkout/PaymentModule.js
+++ src/pages/Checkout/PaymentModule.js
@@ -1,39 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import { STRIPE_PUBLISHABLE_API_KEY} from '../../config/StoreInfo';
-import CheckoutForm from './CheckoutForm';
+import Button from '../../components/Button';
+import { CardElement } from '@stripe/react-stripe-js';
 
 
-const stripePromise = loadStripe(STRIPE_PUBLISHABLE_API_KEY)
-export default function PaymentModule(props){
-  const [clientSecret, setClientSecret] = useState("");
-
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setClientSecret(data.clientSecret)
-      }
-      );
-  }, [setClientSecret]);
-
-  const options = {
-    clientSecret: clientSecret
-  };
-
+const PaymentModule = () =>{
   return (
-    <div className="App">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
+    <div>
+      <h1> Payment </h1>
+      <form id="payment-form">
+        <label htmlFor="card-element"> Card </label>
+        <CardElement id="card-element"/>
+        <Button text={"Pay"} onClick={()=>{console.log("hi")}}/>
+      </form>
     </div>
-  );
+  )
 }
+
+export default PaymentModule;
