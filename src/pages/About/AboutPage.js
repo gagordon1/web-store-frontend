d54d06ed5d0c353da3506c843be1c911619e6380
Garrett Gordon <gagordon@mit.edu>
2022-06-15 00:25:08 -0400

    changing about page

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,8 +1,16 @@
 import { FORD_FOCUS_WIKIPEDIA_DESCRIPTION } from '../../config/StoreInfo'
+import styled from 'styled-components';
+
+const AboutContainer = styled.div`
+  margin-left : auto;
+  margin-right : auto;
+  width : 90%;
+  max-width : 1000px;
+`
 
 export default function AboutPage() {
   return (
-    <div> {FORD_FOCUS_WIKIPEDIA_DESCRIPTION} </div>
+    <AboutContainer> {FORD_FOCUS_WIKIPEDIA_DESCRIPTION} </AboutContainer>
   )
 
 }
