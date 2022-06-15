023b9323105166a534eafb94d4c38885569bf220
Garrett Gordon <gagordon@mit.edu>
2022-06-14 21:33:52 -0400

    development mode working

--- src/config/StoreInfo.js
+++ src/config/StoreInfo.js
@@ -1,9 +1,16 @@
 export const FORD_FOCUS_WIKIPEDIA_DESCRIPTION = "The Ford Focus (first generation) is a compact car that was manufactured by Ford in Europe from 1998 to 2004 and by Ford in North America from 1999 to 2007. Ford began sales of the Focus to Europe in July 1998 and in North America during 1999 for the 2000 model year. Manufacturing in Argentina continued until 2008, and it was still on sale in Brazil until 2009."
 
+export const PRODUCTION = false;
 
 export const STORE_NAME = "Drip Town";
 
-export const STRIPE_PUBLISHABLE_API_KEY= "pk_test_51KQyC1LqSz5uCrfjrBlU5h0pUWPyaeQUF3FQIqrV7t1ZXTbSvQ7hfXYd4YJJipA3C6hR4noImSX1kEmv6BHCRQXg00AjCjsyOs";
+const STRIPE_TEST_PUBLISHABLE_API_KEY= "pk_test_51KQyC1LqSz5uCrfjrBlU5h0pUWPyaeQUF3FQIqrV7t1ZXTbSvQ7hfXYd4YJJipA3C6hR4noImSX1kEmv6BHCRQXg00AjCjsyOs";
+
+const STRIPE_LIVE_PUBLISHABLE_API_KEY="pk_live_51KQyC1LqSz5uCrfjlCnWxsedC5un4vTA3cpKQRihqzzHyLiJfHlb04QDZYWDLco5t8sSJST5jiXIpEi0dgrbNRlT00rODeDoXu";
+
+export const STRIPE_PUBLISHABLE_KEY = PRODUCTION? STRIPE_LIVE_PUBLISHABLE_API_KEY : STRIPE_TEST_PUBLISHABLE_API_KEY
+
+console.log(STRIPE_PUBLISHABLE_KEY)
 
 export const STRIPE_ACCOUNT_ID = "acct_1KQyC1LqSz5uCrfj";
 
@@ -17,5 +24,3 @@ export const ITEM_DESCRIPTIONS  = {
   275969640 : FORD_FOCUS_WIKIPEDIA_DESCRIPTION
 
 }
-
-export const PRODUCTION = true;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js'
-import { STRIPE_PUBLISHABLE_API_KEY } from './config/StoreInfo'
+import { STRIPE_PUBLISHABLE_KEY } from './config/StoreInfo'
 
 
 
 (async () => {
-
-  const stripePromise = loadStripe(STRIPE_PUBLISHABLE_API_KEY);
+  
+  const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
