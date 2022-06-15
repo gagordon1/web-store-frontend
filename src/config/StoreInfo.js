88f178079f8fd72ed7e01e9d8feadd96a59075e3
Garrett Gordon <gagordon@mit.edu>
2022-06-14 23:06:32 -0400

    changed product descriptions, ready for deploy

--- src/config/StoreInfo.js
+++ src/config/StoreInfo.js
@@ -1,6 +1,6 @@
 export const FORD_FOCUS_WIKIPEDIA_DESCRIPTION = "The Ford Focus (first generation) is a compact car that was manufactured by Ford in Europe from 1998 to 2004 and by Ford in North America from 1999 to 2007. Ford began sales of the Focus to Europe in July 1998 and in North America during 1999 for the 2000 model year. Manufacturing in Argentina continued until 2008, and it was still on sale in Brazil until 2009."
 
-export const PRODUCTION = true;
+export const PRODUCTION = false;
 
 export const STORE_NAME = "Drip Town";
 
